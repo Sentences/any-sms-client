17e549ba526b4b8cb99e775644ea16b55a3f9588
Nico <lt500r@gmail.com>
2012-02-11 22:44:33 +0100

    Moeglichkeit die Benachrichtigung in den Settings einzustellen hinzugefuegt.

--- src/de/mxtracks/android/anySMS/Settings.java
+++ src/de/mxtracks/android/anySMS/Settings.java
@@ -7,10 +7,14 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-public class Settings  extends Activity implements OnClickListener{
+public class Settings extends Activity implements OnClickListener,
+		OnCheckedChangeListener {
 	public static final String PREFS_NAME = "MyPrefs";
 	public static final String TAG = "anySMS Settings";
 	private EditText etUser;
@@ -18,67 +22,84 @@ public class Settings  extends Activity implements OnClickListener{
 	private EditText etAbsender;
 	private Spinner spGateway;
 	private Button btnSave;
-	
-	
-	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings);
-        
-        etUser = (EditText) this.findViewById(R.id.etUser);
-        etPassword = (EditText) this.findViewById(R.id.etPassword);
-        etAbsender = (EditText) this.findViewById(R.id.etAbsender);
-        spGateway = (Spinner) this.findViewById(R.id.spinner1);
-        btnSave = (Button) this.findViewById(R.id.btnSave);
-        
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        String userID = settings.getString("userID", null);
-        String userPass = settings.getString("userPass", null);
-        String userAbsender = settings.getString("userAbsender", null);
-        
-        btnSave.setOnClickListener(this);
-        etUser.setText(userID);
-        etPassword.setText(userPass);
-        etAbsender.setText(userAbsender);
-        
-	}
-	
-	@Override
-    protected void onPause(){
-       super.onStop();
+	private CheckBox cbNotify;
 
-      // We need an Editor object to make preference changes.
-      // All objects are from android.context.Context
-      SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-      SharedPreferences.Editor editor = settings.edit();
-      editor.putString("userID", etUser.getText().toString());
-	  editor.putString("userPass", etPassword.getText().toString());
-	  editor.putString("userAbsender", etAbsender.getText().toString());
-	  editor.putString("userGateway", spGateway.getSelectedItem().toString());
-      // Commit the edits!
-      editor.commit();
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings);
+
+		etUser = (EditText) this.findViewById(R.id.etUser);
+		etPassword = (EditText) this.findViewById(R.id.etPassword);
+		etAbsender = (EditText) this.findViewById(R.id.etAbsender);
+		spGateway = (Spinner) this.findViewById(R.id.spinner1);
+		btnSave = (Button) this.findViewById(R.id.btnSave);
+		cbNotify = (CheckBox) this.findViewById(R.id.cbNotify);
+
+		cbNotify.setOnCheckedChangeListener(this);
+
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		String userID = settings.getString("userID", null);
+		String userPass = settings.getString("userPass", null);
+		String userAbsender = settings.getString("userAbsender", null);
+		Boolean userNotify = settings.getBoolean("userNotify", false);
+
+		btnSave.setOnClickListener(this);
+		etUser.setText(userID);
+		etPassword.setText(userPass);
+		etAbsender.setText(userAbsender);
+		cbNotify.setChecked(userNotify);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onStop();
+
+		// We need an Editor object to make preference changes.
+		// All objects are from android.context.Context
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putString("userID", etUser.getText().toString());
+		editor.putString("userPass", etPassword.getText().toString());
+		editor.putString("userAbsender", etAbsender.getText().toString());
+		editor.putString("userGateway", spGateway.getSelectedItem().toString());
+		// Commit the edits!
+		editor.commit();
+	}
 
 	@Override
 	public void onClick(View v) {
-		switch(v.getId()){
+		switch (v.getId()) {
 		case R.id.btnSave:
-			Log.i(TAG,"Save settings");
+			Log.i(TAG, "Save settings");
 			// We need an Editor object to make preference changes.
-		    // All objects are from android.context.Context
-		    SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-		    SharedPreferences.Editor editor = settings.edit();
-		    editor.putString("userID", etUser.getText().toString());
-		    editor.putString("userPass", etPassword.getText().toString());
-		    editor.putString("userAbsender", etAbsender.getText().toString());
-		    editor.putString("userGateway", spGateway.getSelectedItem().toString());
-		    // Commit the edits!
-		    editor.commit();
+			// All objects are from android.context.Context
+			SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putString("userID", etUser.getText().toString());
+			editor.putString("userPass", etPassword.getText().toString());
+			editor.putString("userAbsender", etAbsender.getText().toString());
+			editor.putString("userGateway", spGateway.getSelectedItem()
+					.toString());
+			// Commit the edits!
+			editor.commit();
 			break;
-		
-		
+
 		}
-		
+
 	}
 
-	
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		// TODO Auto-generated method stub
+		switch (buttonView.getId()) {
+		case R.id.cbNotify:
+			SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putBoolean("userNotify", isChecked);
+			editor.commit();
+			break;
+
+		}
+	}
+
 }
